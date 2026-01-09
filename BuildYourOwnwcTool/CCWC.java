592ee62400f8c11ac0247350a18a3e27a5ee7d9f
Parthib Nag <parthibnag@gmail.com>
2026-01-08 17:09:56 -0800

    Completed countWords

--- BuildYourOwnwcTool/CCWC.java
+++ BuildYourOwnwcTool/CCWC.java
@@ -3,15 +3,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner; 
 
 public class CCWC {
     
     public static void main(String[] args){
-        System.out.println(countLines("test.txt"));
+        System.out.println(countWords("test.txt"));
     }
 
     private static long countBytes(String fileName) {
-        // TODO: Count bytes
         File file = new File(fileName);
         if (file.exists()){
             return file.length();
@@ -36,14 +37,20 @@ public class CCWC {
 
     private static long countWords(String fileName){
         long count = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
-            while(br.readLine() != null) {
+
+        try{
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNext()){
+                scanner.next();
                 count++;
             }
+            scanner.close();
         }
-        catch (IOException e){
-            System.out.println("Error reading file.");
+        catch (FileNotFoundException e){
+            System.err.println("File not found: " + fileName);
         }
+
         return count;
     }
 }
